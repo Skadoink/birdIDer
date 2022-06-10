c252e28bc2382f6324f1a8c4456180958ea7bb71
ehrharoska <ehrharoska@vuw.ac.nz>
2022-06-11 00:16:57 +1200

    tried video object detection, but is too slow

--- professions.py
+++ professions.py
@@ -1,6 +1,8 @@
 from imageai.Classification.Custom import ClassificationModelTrainer
+import tensorflow as tf
 
 model_trainer = ClassificationModelTrainer()
 model_trainer.setModelTypeAsResNet50()
 model_trainer.setDataDirectory("idenprof")
-model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
+with tf.device('/device:GPU:0'):
+    model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
