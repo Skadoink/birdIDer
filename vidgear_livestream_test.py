c00e86f7ab36d1e65d172038c95f19a5b9f885df
ehrharoska <ehrharoska@vuw.ac.nz>
2022-06-21 00:42:56 +1200

    fixed so saving one frame per second. Issue with time() is vidgear freezing. Not sure if current solution is good long term yet. Otherwise will need gstream.

--- vidgear_livestream_test.py
+++ vidgear_livestream_test.py
@@ -1,5 +1,5 @@
 # import libraries
-from vidgear.gears import CamGear 
+from vidgear.gears import CamGear
 # Getting a frame every second for scanning with AI.
 # This allows the program to keep up with the live feed's pace
 
@@ -13,31 +13,36 @@ def scan_image(frame):
     # if object found, save frame and log result.
     return
 
+
 def birdIDer():
     fpsLimit = 1  # throttle limit
     startTime = time.time()
     imagesFolder = "saved_images"
     x = 1
+    c = 0
 
     # Create an object to hold reference to livestream
     #cap = cv2.VideoCapture(0)
-    options = {"CAP_PROP_FPS":1}
-    cap = CamGear(source='https://www.youtube.com/watch?v=JhajwzEv1Fo', stream_mode = True, logging=True, **options).start()
+    options = {"CAP_PROP_FPS": 1}
+    cap = CamGear(source='https://www.youtube.com/watch?v=JhajwzEv1Fo',
+                  stream_mode=True, logging=True, **options).start()
+    # currently only doing one frame per 5 seconds.
 
     while True:
         nowTime = time.time()
-        frame = cap.read()  # capture a frame from live video
-        if (int(nowTime - startTime)) > fpsLimit:
-            # check whether frame is successfully captured
-            # TODO: unique naming might be needed
-            filename = imagesFolder + "/image_" + str(x) + ".jpg"; x += 1
-            # save image, may need to be removed later.
-            cv2.imwrite(filename, frame)
+        frame = cap.read()  # capture the frame from live video
+        # if (nowTime - startTime) >= fpsLimit: #without this, every single frame is saved even though options state 1fps
+        if(c % 30 == 0):
+            filename = imagesFolder + "/image_" + str(x) + ".jpg"
+            x += 1
+            cv2.imwrite(filename, frame)  # save image
             scan_image(frame)
-
             startTime = time.time()  # reset time
+        c += 1
+
 
 def main():
     birdIDer()
+
 
 main()
