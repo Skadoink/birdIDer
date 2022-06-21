266ce5d1d61754bb3a5fc8a230cc9bdece400f9a
ehrharoska <ehrharoska@vuw.ac.nz>
2022-06-21 16:28:25 +1200

    multiprocessing implemented for scan_image() calls

--- vidgear_livestream_test.py
+++ vidgear_livestream_test.py
@@ -1,48 +1,51 @@
 # import libraries
-from vidgear.gears import CamGear
 # Getting a frame every second for scanning with AI.
 # This allows the program to keep up with the live feed's pace
-
-import cv2  # include opencv library functions in python
 import time  # for sleep
+import multiprocessing
 
 
 # TODO: object detection function
-def scan_image(frame):
+def scan_image(frame, call_time):
     # run AI on frame
     # if object found, save frame and log result.
+    print("we are in scan_image()! count: " + str(frame))
     return
 
 
-def birdIDer():
-    fpsLimit = 1  # throttle limit
-    startTime = time.time()
-    imagesFolder = "saved_images"
-    x = 1
-    c = 0
+if __name__ == '__main__':
+    import cv2  # include opencv library functions in python
+    from vidgear.gears import CamGear
 
-    # Create an object to hold reference to livestream
-    #cap = cv2.VideoCapture(0)
-    options = {"CAP_PROP_FPS": 1}
-    cap = CamGear(source='https://www.youtube.com/watch?v=JhajwzEv1Fo',
-                  stream_mode=True, logging=True, **options).start()
-    # currently only doing one frame per 5 seconds.
+    def birdIDer():
+        fpsLimit = 1  # throttle limit
+        startTime = time.time()
+        imagesFolder = "saved_images"
+        x = 1
+        c = 0
 
-    while True:
-        nowTime = time.time()
-        frame = cap.read()  # capture the frame from live video
-        # if (nowTime - startTime) >= fpsLimit: #without this, every single frame is saved even though options state 1fps
-        if(c % 30 == 0):
-            filename = imagesFolder + "/image_" + str(x) + ".jpg"
-            x += 1
-            cv2.imwrite(filename, frame)  # save image
-            scan_image(frame)
-            startTime = time.time()  # reset time
-        c += 1
+        # Create an object to hold reference to livestream
+        #cap = cv2.VideoCapture(0)
+        options = {"CAP_PROP_FPS": 1}
+        cap = CamGear(source='https://www.youtube.com/watch?v=JhajwzEv1Fo',
+                      stream_mode=True, logging=True, **options).start()
+        # currently only doing one frame per 5 seconds.
 
+        while True:
+            nowTime = time.time()
+            frame = cap.read()  # capture the frame from live video
+            # if (nowTime - startTime) >= fpsLimit: #without this, every single frame is saved even though options state 1fps
+            if(c % 30 == 0):
+                filename = imagesFolder + "/image_" + str(x) + ".jpg"
+                x += 1
+                cv2.imwrite(filename, frame)  # save image
+                # new object detection process per frame
+                p = multiprocessing.Process(target=scan_image(x))
+                p.start()
+                startTime = time.time()  # reset time
+            c += 1
 
-def main():
-    birdIDer()
+    def main():
+        birdIDer()
 
-
-main()
+    main()
