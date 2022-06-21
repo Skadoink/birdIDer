31a22d57a1e235cede78ef6d873b7f65acca31bf
ehrharoska <ehrharoska@vuw.ac.nz>
2022-06-21 16:42:49 +1200

    cleaned

--- vidgear_livestream_test.py
+++ vidgear_livestream_test.py
@@ -1,12 +1,11 @@
-# import libraries
+# import libraries used in child processes and/or main
 # Getting a frame every second for scanning with AI.
 # This allows the program to keep up with the live feed's pace
-import time  # for sleep
 import multiprocessing
 
 
 # TODO: object detection function
-def scan_image(frame, call_time):
+def scan_image(frame):
     # run AI on frame
     # if object found, save frame and log result.
     print("we are in scan_image()! count: " + str(frame))
@@ -14,12 +13,11 @@ def scan_image(frame, call_time):
 
 
 if __name__ == '__main__':
+    # import libraries used only in main
     import cv2  # include opencv library functions in python
     from vidgear.gears import CamGear
 
     def birdIDer():
-        fpsLimit = 1  # throttle limit
-        startTime = time.time()
         imagesFolder = "saved_images"
         x = 1
         c = 0
@@ -32,17 +30,15 @@ if __name__ == '__main__':
         # currently only doing one frame per 5 seconds.
 
         while True:
-            nowTime = time.time()
             frame = cap.read()  # capture the frame from live video
             # if (nowTime - startTime) >= fpsLimit: #without this, every single frame is saved even though options state 1fps
             if(c % 30 == 0):
                 filename = imagesFolder + "/image_" + str(x) + ".jpg"
-                x += 1
                 cv2.imwrite(filename, frame)  # save image
                 # new object detection process per frame
                 p = multiprocessing.Process(target=scan_image(x))
                 p.start()
-                startTime = time.time()  # reset time
+                x += 1
             c += 1
 
     def main():
