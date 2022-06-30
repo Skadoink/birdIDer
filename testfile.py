600e9a0584da00b5bbf2693ccb11d93df3135cbe
ehrharoska <ehrharoska@vuw.ac.nz>
2022-06-30 13:17:09 +1200

    imageai still not working for object detection. tried training and using a bird model, and changing to py3.7.6

--- testfile.py
+++ testfile.py
@@ -1,51 +1,15 @@
-# import libraries
-from vidgear.gears import CamGear
-# Getting a frame every second for scanning with AI.
-# This allows the program to keep up with the live feed's pace
+from imageai.Classification.Custom import CustomImageClassification
+import os
 
-import cv2  # include opencv library functions in python
-import time  # for sleep
-import multiprocessing
+execution_path = os.getcwd()
 
+prediction = CustomImageClassification()
+prediction.setModelTypeAsResNet50()
+prediction.setModelPath("model_b.h5")
+prediction.setJsonPath("model_class_b.json")
+prediction.loadModel(num_objects=3)
 
-# TODO: object detection function
-def scan_image(frame):
-    # run AI on frame
-    # if object found, save frame and log result.
-    print("we are in scan_image()!" + str(frame))
-    return
+predictions, probabilities = prediction.classifyImage("House-sparrow.jpeg", result_count=3)
 
-# if __name__ == '__main__':
-def birdIDer():
-    fpsLimit = 1  # throttle limit
-    startTime = time.time()
-    imagesFolder = "saved_images"
-    x = 1
-    c = 0
-
-    # Create an object to hold reference to livestream
-    #cap = cv2.VideoCapture(0)
-    options = {"CAP_PROP_FPS": 1}
-    cap = CamGear(source='https://www.youtube.com/watch?v=JhajwzEv1Fo',
-                stream_mode=True, logging=True, **options).start()
-    # currently only doing one frame per 5 seconds.
-
-    while True:
-        nowTime = time.time()
-        frame = cap.read()  # capture the frame from live video
-        # if (nowTime - startTime) >= fpsLimit: #without this, every single frame is saved even though options state 1fps
-        if(c % 30 == 0):
-            filename = imagesFolder + "/image_" + str(x) + ".jpg"; x += 1
-            cv2.imwrite(filename, frame)  # save image
-            # p = multiprocessing.Process(target = scan_image(x)) # object detection process per frame
-            # p.start()
-            # p.join()
-            startTime = time.time()  # reset time
-        c += 1
-
-
-def main():
-    birdIDer()
-
-
-main()
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
