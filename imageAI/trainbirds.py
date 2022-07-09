ab24b99f7e90c384848d7c51179dd487eb188de2
ehrharoska <ehrharoska@vuw.ac.nz>
2022-07-09 21:37:13 +1200

    untracked old imageAI files (cleaning repo)

--- imageAI/trainbirds.py
+++
@@ -1,8 +0,0 @@
-from imageai.Classification.Custom import ClassificationModelTrainer
-import tensorflow as tf
-
-model_trainer = ClassificationModelTrainer()
-model_trainer.setModelTypeAsResNet50()
-model_trainer.setDataDirectory("birds")
-with tf.device('/device:GPU:0'):
-    model_trainer.trainModel(num_objects=3, num_experiments=100, enhance_data=True, batch_size=32, show_network_summary=True)
